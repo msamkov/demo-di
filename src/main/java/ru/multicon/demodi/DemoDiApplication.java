b151e7d81ea61ce1b17f93767b2d570c04b59393
Max Samkov <littlebiggame2@gmail.com>
2018-05-04 19:12:22 +0300

    Get Been myController from Application context

--- src/main/java/ru/multicon/demodi/DemoDiApplication.java
+++ src/main/java/ru/multicon/demodi/DemoDiApplication.java
@@ -2,11 +2,19 @@ package ru.multicon.demodi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.multicon.demodi.controllers.MyController;
+import sun.awt.AppContext;
 
+/**
+ *  Created by msamkov on 04.05.2018
+ */
 @SpringBootApplication
 public class DemoDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DemoDiApplication.class, args);
+		MyController controller = (MyController) ctx.getBean("myController");
+		controller.hello();
 	}
 }
---
+++ src/main/java/ru/multicon/demodi/controllers/MyController.java
@@ -0,0 +1,16 @@
+
+package ru.multicon.demodi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *  Created by msamkov on 04.05.2018
+ */
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!!");
+        return "foo";
+    }
+}
