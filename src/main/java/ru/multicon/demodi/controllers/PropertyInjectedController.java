971920ffe31472f5d04ca5afc0ab6fbca545259f
Max Samkov <littlebiggame2@gmail.com>
2018-05-17 15:58:28 +0300

    Dependency Injection without the Spring Framework:   - Constructor Injected   - Setter Injected   - Property Injected

--- src/main/java/ru/multicon/demodi/controllers/PropertyInjectedController.java
+++ src/main/java/ru/multicon/demodi/controllers/PropertyInjectedController.java
@@ -1,13 +1,13 @@
 package ru.multicon.demodi.controllers;
 
-import ru.multicon.demodi.services.GreetingServiceImpl;
+import ru.multicon.demodi.services.GreetingService;
 
 /**
  * Created by msamkov on 17.05.2018
  */
 public class PropertyInjectedController {
 
-    public GreetingServiceImpl greetingService;
+    public GreetingService greetingService;
 
     String sayHello() {
         return greetingService.sayGreeting();
